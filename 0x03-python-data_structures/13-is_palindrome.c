c17a5f34c3d32ae1d167fc8ea0ff19b29acdd60c
faithadewuyi <iyanuleke09@gmail.com>
2023-11-06 03:18:13 -0800

    Corrected

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,13 +3,42 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * is_palindrome - function that checks holberton
  * Return: Always 0.
  */
-int main(void)
-{
-	listint_t *head;
 
-	return (0);
+int is_palindrome(listint_t **head)
+{
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *temp;
+
+    while (fast != NULL && fast->next != NULL) 
+{
+        fast = fast->next->next;
+
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    if (fast != NULL)
+ {
+        slow = slow->next;
+    }
+
+  
+    while (prev != NULL && slow != NULL) {
+        if (prev->n != slow->n)
+ {
+            return (0);  
+        }
+        prev = prev->next;
+        slow = slow->next;
+    }
+
+    return (1); 
 }
+
